b7dcbb991d31a3136eec48999ae84611ccf6f241
Hristo Nikolov <Hristo.Nikolov@Honeywell.com>
2017-04-21 00:00:05 +0200

    Work in Progress on GPU envelop <STX> <PLD...> <ETX> <BCC> with echo.

--- workspace/MSP-EXP430FR5739 SPET/src/GPU.c
+++ workspace/MSP-EXP430FR5739 SPET/src/GPU.c
@@ -1,19 +1,28 @@
+/*******************************************************************************
+ * GPU envelop:
+ * <STX> <PLD> <ETX> <BCC>
+ * <PLD> can be several characters
+ * Len determined by <ETX> (0x03)
+ * BCC = XOR ( <PLD> <ETX> ); 7 bits
+ *
+ ******************************************************************************/
+
 #include "GPU.h"
 #include "FR_EXP.h"
 
 
-static char GPU_Buffer[3] = {0};
-
+static char GPU_Buffer[BUFF_SIZE] = {0};
+static unsigned int ui_Cntr = 0;
 
 typedef enum
 {
-    STX = 0,
-    PLD,
-    BCC,
-    ETX
+    st_STX = 0,
+    st_PLD,
+    st_BCC
+
 } my_state_t;
 
-my_state_t state = STX;
+my_state_t state = st_STX;
 
 void GPU_Rx( char input )
 {
@@ -21,42 +30,40 @@ void GPU_Rx( char input )
 
     switch(state)
     {
-        case STX:
-            if( input == 's' )
+        case st_STX:
+            if( input == STX )
             {
-                GPU_Buffer[0] = input;
-                state = PLD;
-            }
-            else
-            {
-                state = STX;
+                ui_Cntr = 0;
+                GPU_Buffer[ui_Cntr] = input;
+                ui_Cntr++;
+                state = st_PLD;
             }
             break;
 
-        case PLD:
-            if( input == 's' ) // TODO: Proper const
-            {
-                GPU_Buffer[0] = input;
-                state = PLD;
-            }
-            else
-            {
-                GPU_Buffer[1] = input;
-                state = ETX;
-            }
+        case st_PLD:
+            // This should not happen
+            if( ui_Cntr > BUFF_SIZE-1 )      { state = st_STX; }
+
+            // Read STX data; stay at PLD
+            if( input == STX )               { ui_Cntr = 0;    }
+
+            // Read ETX data; switch to BCC
+            else if( input == ETX )          { state = st_BCC; }
+
+            GPU_Buffer[ui_Cntr] = input;
+            ui_Cntr++;
+
             break;
 
-        case ETX:
-            if( input == 'e' )
-            {
-                GPU_Buffer[2] = input;
-                GPU_RX_OK = 1;
-            }
-            state = STX; // Protocol message received
+        case st_BCC:
+            GPU_Buffer[ui_Cntr] = input;
+            ui_Cntr++;
+            GPU_RX_OK = 1;
+            state = st_STX; // Protocol message received
             break;
 
-        default:
-            state = STX;
+        default: // Unknown state; Should not happen
+            state = st_STX;
             break;
     }
 }
@@ -65,7 +72,7 @@ void GPU_Rx( char input )
 void GPU_Tx()
 {
     UART_TX_Char('\n');
-    UART_TX_Data(GPU_Buffer, 3);
+    UART_TX_Data(GPU_Buffer, ui_Cntr);
     UART_TX_Char('\n');
 }
 
--- workspace/MSP-EXP430FR5739 SPET/src/GPU.h
+++ workspace/MSP-EXP430FR5739 SPET/src/GPU.h
@@ -4,6 +4,14 @@
 #ifndef GPU_INCLUDED
 #define GPU_INCLUDED
 
+#define STX 's'
+#define ETX 'e'
+//#define STX (0x02)
+//#define ETX (0x03)
+//#define ACK (0x06)
+
+#define BUFF_SIZE 10
+
 volatile unsigned int GPU_RX_OK;
 
 void GPU_Rx( char input );
